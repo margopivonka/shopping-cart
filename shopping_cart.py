8f7159836f6bde523ad47a6aa850e75b46886606
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-02-07 16:04:20 -0500

    seperate capture and display processes

--- shopping_cart.py
+++ shopping_cart.py
@@ -38,7 +38,7 @@ products = [
 #
 
 total_price = 0
-
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ")
@@ -47,12 +47,7 @@ while True:
         break
         #break keyword will stop a loop
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        #have to convert input to string so that code can match the items since ist id is an int
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
+        selected_ids.append(selected_id)
 
 
 
@@ -60,7 +55,24 @@ while True:
 # INFO DISPLAY / OUTPUT
 #
 
- 
+#print(selected_ids)
+
+
+for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+            #have to convert input to string so that code can match the items since ist id is an int
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+#print("----------------------")
+#print("MARGO'S GROCERY STORE") 
+#print("----------------------")
+#
+#print("Web: www.margos.com")
+#print("Phone: (216)-112-1357")
+#print("Checkout time: ")
+
 print("TOTAL PRICE: " + str(total_price)) #TODO format as USD
 
 
