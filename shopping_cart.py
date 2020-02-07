015d9b6304baedb9c8ed5fa46f6a1b54d7a22c2d
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-02-07 15:48:28 -0500

    using if loop to let user break loop if the enter done

--- shopping_cart.py
+++ shopping_cart.py
@@ -37,12 +37,17 @@ products = [
 # INFO CAPTURE / INPUT
 #
 
-selected_id = input("Please input a product identifier: ")
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-    #have to convert input to string so that code can match the items since ist id is an int
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
+while True:
+    selected_id = input("Please input a product identifier: ")
+    
+    if selected_id == "DONE":
+        break
+        #break keyword will stop a loop
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #have to convert input to string so that code can match the items since ist id is an int
+        matching_product = matching_products[0]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
 
