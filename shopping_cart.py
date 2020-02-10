f0bb9a15dbf4f39a9299f621096993a39a3dfe6b
margopivonka <59657652+margopivonka@users.noreply.github.com>
2020-02-10 14:47:18 -0500

    Final Version

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,9 +1,6 @@
-#print ("shopping cart")
+import datetime as dt
 
-
-
-
-
+TAX_RATE = 0.06
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -37,12 +34,12 @@ products = [
 # INFO CAPTURE / INPUT
 #
 
-total_price = 0
+checkout_time = dt.datetime.now()
+subtotal = 0
 selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ")
-    
     if selected_id == "DONE":
         break
         #break keyword will stop a loop
@@ -55,26 +52,44 @@ while True:
 # INFO DISPLAY / OUTPUT
 #
 
-#print(selected_ids)
+print("----------------------")
+print("MARGO'S GROCERY STORE") 
+print("----------------------")
+print(" ")
+print("Web: www.margos.com")
+print("Phone: (216)-112-1357")
+print("Checkout time: " + checkout_time.strftime("%Y-%m-%d %I:%M %p"))
+print("----------------------")
+print(" ")
 
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+print("SELECTED PRODUCTS: ")
 
 for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
             #have to convert input to string so that code can match the items since ist id is an int
         matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        subtotal = subtotal + int(matching_product["price"])
+        print(matching_product["name"] + " " + to_usd(matching_product["price"]))
 
-#print("----------------------")
-#print("MARGO'S GROCERY STORE") 
-#print("----------------------")
-#
-#print("Web: www.margos.com")
-#print("Phone: (216)-112-1357")
-#print("Checkout time: ")
+tax = subtotal * TAX_RATE
 
-print("TOTAL PRICE: " + str(total_price)) #TODO format as USD
+total = subtotal + tax
 
+print("----------------------")
+print(" ")
+print("SUBTOTAL: " + to_usd(subtotal))
+print("TAX: " + to_usd(tax))
+print("TOTAL: " + to_usd(total))
+
+print(" ")
+print("----------------------")
+print("THANKS, SEE YOU AGAIN")
+print("----------------------")
+print(" ")
 
 
 
